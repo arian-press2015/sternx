5b7dd068e0a77681c2320c3daec63b3e6565078d
AP2015 <arian.press2015@gmail.com>
2023-08-11 06:51:08 +0330

    feat: add basic GatewayService and tests to ashland

--- ashland/src/gateway/gateway.service.spec.ts
+++ ashland/src/gateway/gateway.service.spec.ts
@@ -15,4 +15,44 @@ describe('GatewayService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('createTaskEvent', () => {
+    it('should be defined', () => {
+      expect(service.createTaskEvent).toBeDefined();
+    });
+
+    it('should return true', async () => {
+      expect(await service.createTaskEvent({})).toEqual(true);
+    });
+  });
+
+  describe('findOneTaskEvent', () => {
+    it('should be defined', () => {
+      expect(service.findOneTaskEvent).toBeDefined();
+    });
+
+    it('should return true', async () => {
+      expect(await service.findOneTaskEvent({})).toEqual(true);
+    });
+  });
+
+  describe('findAllTaskEvent', () => {
+    it('should be defined', () => {
+      expect(service.findAllTasksEvent).toBeDefined();
+    });
+
+    it('should return true', async () => {
+      expect(await service.findAllTasksEvent({})).toEqual(true);
+    });
+  });
+
+  describe('deleteTaskEvent', () => {
+    it('should be defined', () => {
+      expect(service.deleteTaskEvent).toBeDefined();
+    });
+
+    it('should return true', async () => {
+      expect(await service.deleteTaskEvent({})).toEqual(true);
+    });
+  });
 });
--- ashland/src/gateway/gateway.service.ts
+++ ashland/src/gateway/gateway.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GatewayService {}
+export class GatewayService {
+  async createTaskEvent(data: Record<string, unknown>) {
+    console.log(data);
+    return true;
+  }
+
+  async findOneTaskEvent(data: Record<string, unknown>) {
+    console.log(data);
+    return true;
+  }
+
+  async findAllTasksEvent(data: Record<string, unknown>) {
+    console.log(data);
+    return true;
+  }
+
+  async deleteTaskEvent(data: Record<string, unknown>) {
+    console.log(data);
+    return true;
+  }
+}
