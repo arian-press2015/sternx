2efebf3739f21b3f869ee50cd5a3b490d073cb90
AP2015 <arian.press2015@gmail.com>
2023-08-11 07:02:06 +0330

    feat: add basic GatewayController and tests to ashland

--- ashland/src/gateway/gateway.controller.spec.ts
+++ ashland/src/gateway/gateway.controller.spec.ts
@@ -5,10 +5,22 @@ import { GatewayService } from './gateway.service';
 describe('GatewayController', () => {
   let controller: GatewayController;
 
+  const GatewayServiceMock = {
+    createTaskEvent: jest.fn().mockResolvedValue(true),
+    findOneTaskEvent: jest.fn().mockResolvedValue(true),
+    findAllTasksEvent: jest.fn().mockResolvedValue(true),
+    deleteTaskEvent: jest.fn().mockResolvedValue(true),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GatewayController],
-      providers: [GatewayService],
+      providers: [
+        {
+          provide: GatewayService,
+          useValue: GatewayServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<GatewayController>(GatewayController);
@@ -16,5 +28,53 @@ describe('GatewayController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createTaskEvent', () => {
+    it('should be defined', () => {
+      expect(controller.createTaskEvent).toBeDefined();
+    });
+
+    it('should call GatewayService', async () => {
+      expect(await controller.createTaskEvent({})).toEqual(true);
+
+      expect(GatewayServiceMock.createTaskEvent).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneTaskEvent', () => {
+    it('should be defined', () => {
+      expect(controller.findOneTaskEvent).toBeDefined();
+    });
+
+    it('should call GatewayService', async () => {
+      expect(await controller.findOneTaskEvent({})).toEqual(true);
+
+      expect(GatewayServiceMock.findOneTaskEvent).toHaveBeenCalled();
+    });
+  });
+
+  describe('findAllTaskEvent', () => {
+    it('should be defined', () => {
+      expect(controller.findAllTasksEvent).toBeDefined();
+    });
+
+    it('should call GatewayService', async () => {
+      expect(await controller.findAllTasksEvent({})).toEqual(true);
+
+      expect(GatewayServiceMock.findAllTasksEvent).toHaveBeenCalled();
+    });
+  });
+
+  describe('deleteTaskEvent', () => {
+    it('should be defined', () => {
+      expect(controller.deleteTaskEvent).toBeDefined();
+    });
+
+    it('should call GatewayService', async () => {
+      expect(await controller.deleteTaskEvent({})).toEqual(true);
+
+      expect(GatewayServiceMock.deleteTaskEvent).toHaveBeenCalled();
+    });
   });
 });
--- ashland/src/gateway/gateway.controller.ts
+++ ashland/src/gateway/gateway.controller.ts
@@ -1,7 +1,28 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 import { GatewayService } from './gateway.service';
 
 @Controller()
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
+
+  @EventPattern('create-task')
+  async createTaskEvent(data: Record<string, unknown>) {
+    return this.gatewayService.createTaskEvent(data);
+  }
+
+  @EventPattern('findOne-task')
+  async findOneTaskEvent(data: Record<string, unknown>) {
+    return this.gatewayService.findOneTaskEvent(data);
+  }
+
+  @EventPattern('findAll-tasks')
+  async findAllTasksEvent(data: Record<string, unknown>) {
+    return this.gatewayService.findAllTasksEvent(data);
+  }
+
+  @EventPattern('delete-task')
+  async deleteTaskEvent(data: Record<string, unknown>) {
+    return this.gatewayService.deleteTaskEvent(data);
+  }
 }
