da28b00fa8279994ec043b539e5dcacc30b77f56
AP2015 <arian.press2015@gmail.com>
2023-08-11 08:31:57 +0330

    feat: add basic gatewayController and tests to gallatin

--- gallatin/src/gateway/gateway.controller.spec.ts
+++ gallatin/src/gateway/gateway.controller.spec.ts
@@ -2,13 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 
+const taskMock = {
+  id: 0,
+  parentId: null,
+  title: 'my title',
+  description: 'my description',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
 describe('GatewayController', () => {
   let controller: GatewayController;
+
+  const GatewayServiceMock = {
+    createTask: jest.fn().mockResolvedValue(taskMock),
+    findOneTask: jest.fn().mockResolvedValue(taskMock),
+    findAllTasks: jest.fn().mockResolvedValue([taskMock]),
+    deleteTask: jest.fn().mockResolvedValue(true),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GatewayController],
-      providers: [GatewayService],
+      providers: [
+        {
+          provide: GatewayService,
+          useValue: GatewayServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<GatewayController>(GatewayController);
@@ -16,5 +37,61 @@ describe('GatewayController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createTask', () => {
+    it('should be defined', () => {
+      expect(controller.createTask).toBeDefined();
+    });
+
+    it('should return a task', async () => {
+      expect(
+        await controller.createTask(
+          {
+            title: 'hi',
+            description: 'bye',
+          },
+          {},
+        ),
+      ).toEqual(taskMock);
+
+      expect(GatewayServiceMock.createTask).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOneTask', () => {
+    it('should be defined', () => {
+      expect(controller.findOneTask).toBeDefined();
+    });
+
+    it('should return a task', async () => {
+      expect(await controller.findOneTask(1, {})).toEqual(taskMock);
+
+      expect(GatewayServiceMock.findOneTask).toHaveBeenCalled();
+    });
+  });
+
+  describe('findALlTask', () => {
+    it('should be defined', () => {
+      expect(controller.findAllTasks).toBeDefined();
+    });
+
+    it('should return all tasks', async () => {
+      expect(await controller.findAllTasks({}, {})).toEqual([taskMock]);
+
+      expect(GatewayServiceMock.findAllTasks).toHaveBeenCalled();
+    });
+  });
+
+  describe('deleteTask', () => {
+    it('should be defined', () => {
+      expect(controller.deleteTask).toBeDefined();
+    });
+
+    it('should delete one task', async () => {
+      expect(await controller.deleteTask(2, {})).toEqual(true);
+
+      expect(GatewayServiceMock.deleteTask).toHaveBeenCalled();
+    });
   });
 });
--- gallatin/src/gateway/gateway.controller.ts
+++ gallatin/src/gateway/gateway.controller.ts
@@ -1,7 +1,29 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { CreateTaskDto, FindAllDto, Task } from './dto';
 import { GatewayService } from './gateway.service';
 
 @Controller()
 export class GatewayController {
   constructor(private readonly gatewayService: GatewayService) {}
+
+  @GrpcMethod('create')
+  createTask(data: CreateTaskDto, metadata: any): Promise<Task> {
+    return this.gatewayService.createTask(data);
+  }
+
+  @GrpcMethod('findOne')
+  findOneTask(id: number, metadata: any): Promise<Task> {
+    return this.gatewayService.findOneTask(id);
+  }
+
+  @GrpcMethod('findAll')
+  findAllTasks(data: FindAllDto, metadata: any): Promise<Task[]> {
+    return this.gatewayService.findAllTasks(data);
+  }
+
+  @GrpcMethod('delete')
+  deleteTask(id: number, metadata: any): Promise<boolean> {
+    return this.gatewayService.deleteTask(id);
+  }
 }
